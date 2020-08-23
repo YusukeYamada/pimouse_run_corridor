f982d7d9f4585485747b091d7afc2d3589756bfc
Yusuke Yamada <yusukeyamada0408@outlook.jp>
2020-08-23 15:06:12 +0000

    Change the setup way

--- test/travis_test_wall_trace.py
+++ test/travis_test_wall_trace.py
@@ -18,7 +18,7 @@ class WallStopTest(unittest.TestCase):
         return left, right
 
     def test_io(self):
-        left, right = self.set_and_get(400,0,0,100) #total: 600
+        left, right = self.set_and_get(400,0,0,100) #total: 500
         self.assertTrue(left == right == 0,"can't stop")
 
         left, right = self.set_and_get(0,5,1000,0) #side direction is not a trigger of stop
